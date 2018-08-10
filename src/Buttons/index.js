9aabf49bac0865d56e753260840a2f50c616e949
Martin Ek <mail@ekmartin.com>
2018-08-10 15:53:53 +0200

    Add GitHub link and mobile styling

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,6 +2,7 @@
 import styles from './Buttons.module.css';
 import CopyIcon from '-!svg-react-loader!./copy.svg';
 import TweetIcon from '-!svg-react-loader!./twitter.svg';
+import GitHubIcon from '-!svg-react-loader!./github.svg';
 import React, { Component } from 'react';
 
 const TWEET_URL = 'https://twitter.com/intent/tweet';
@@ -24,13 +25,22 @@ export default class Buttons extends Component {
     const url = `${TWEET_URL}?text=${encoded}`;
     return (
       <div className={styles.buttons}>
-        <button onClick={this.onCopy} className={styles.copy}>
-          <CopyIcon />
-          Copy
-        </button>
-        <a href={url} className={styles.tweet}>
-          <TweetIcon />
-          Tweet
+        <div className={styles.actions}>
+          <button onClick={this.onCopy} className={styles.copy}>
+            <CopyIcon />
+            Copy
+          </button>
+          <a href={url} className={styles.tweet}>
+            <TweetIcon />
+            Tweet
+          </a>
+        </div>
+
+        <a
+          className={styles.github}
+          href="https://github.com/ekmartin/unicode-style"
+        >
+          <GitHubIcon /> unicode-style
         </a>
       </div>
     );
